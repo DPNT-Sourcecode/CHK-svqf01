e57aa2868334501f12ff14964fbf5977a138a693
sbx_user1051 <sbx_user1051@169.254.53.37>
2023-12-07 14:52:39 +0000

    Thu Dec 07 14:52:39 UTC 2023

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -6,7 +6,15 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        validateInput(x);
+        validateInput(y);
+        return x + y;
+    }
+
+    private void validateInput(int input){
+        if(input < 0 || input > 100)
+            throw new RuntimeException();
     }
 
 }
+
--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
 public class SumSolutionTest {
     private SumSolution sum;
@@ -19,4 +20,22 @@ public class SumSolutionTest {
     public void compute_sum() {
         assertThat(sum.compute(1, 1), equalTo(2));
     }
+
+    @Test
+    public void compute_sum_invalid_digits(){
+        assert(sum.compute(101, 5), instanceOf(RuntimeException.class));
+        assertThat(sum.compute(5, 101), instanceOf(RuntimeException.class));
+        assertThat(sum.compute(101, 101), instanceOf(RuntimeException.class));
+
+        assertThat(sum.compute(-5, 1), instanceOf(RuntimeException.class));
+        assertThat(sum.compute(1, -5), instanceOf(RuntimeException.class));
+        assertThat(sum.compute(-5, -5), instanceOf(RuntimeException.class));
+
+        assertThat(sum.compute(101, -5), instanceOf(RuntimeException.class));
+
+
+    }
+
+
 }
+
