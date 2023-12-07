a0a5a7b9d8b430d54d6b2f069e11153032c2b33b
sbx_user1051 <sbx_user1051@169.254.53.37>
2023-12-07 15:02:57 +0000

    Thu Dec 07 15:02:57 UTC 2023

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -3,7 +3,10 @@ package befaster.solutions.HLO;
 import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
+
+    private static final String HELLO_TO_THE_WORLD_MESSAGE = "Hello to the world";
+
     public String hello(String friendName) {
-        throw new SolutionNotImplementedException();
+        return HELLO_TO_THE_WORLD_MESSAGE;
     }
 }
--- src/test/java/befaster/solutions/HLO/HelloWorldTest.java
+++ src/test/java/befaster/solutions/HLO/HelloWorldTest.java
@@ -1,2 +1,25 @@
-package befaster.solutions;public class HLO {
+package befaster.solutions.HLO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import befaster.solutions.TST.One;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HelloWorldTest {
+
+    private HelloSolution helloSolution;
+
+    @BeforeEach
+    public void name() {
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    public void runHello() {
+        assertThat(helloSolution.hello("wtv"), equalTo("Hello to the world"));
+    }
+
+
 }
