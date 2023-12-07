fc4cbb3ccc9229389268b727b4e2ac7df64e4d96
sbx_user1051 <sbx_user1051@169.254.42.37>
2023-12-07 17:31:05 +0000

    Thu Dec 07 17:31:05 UTC 2023

--- src/main/java/befaster/solutions/CHK/inventoryitems/ItemE.java
+++ src/main/java/befaster/solutions/CHK/inventoryitems/ItemE.java
@@ -9,9 +9,10 @@ public class ItemE extends Sku {
     private static final Map<Integer,Integer> DISCOUNT_PAIRS = Map.of(2, ItemB.BASE_COST);
 
     public ItemE() {
-        super(BASE_COST, SkuTypes.A, DISCOUNT_PAIRS);
+        super(BASE_COST, SkuTypes.E, DISCOUNT_PAIRS);
     }
 
 }
+
 
 
